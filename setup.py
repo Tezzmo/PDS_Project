001632efd9fd380012238b94a629b5d558e038b4
Tezzmo <lorenz.kriehn@gmail.com>
2020-04-26 12:10:19 +0200

    First setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='PDS_Project',
-    version='0.0.1dev1',
+    version='0.0.1.dev8',
     description="Semester Project - Programming Data Science",
     author="Student",
     author_email="student@uni-koeln.de",
