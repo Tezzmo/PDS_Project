72cd257576756a1775d96e6dc21f6e337f5580e0
Jonas Froehlich <jonas-froehlich@gmx.net>
2020-05-02 00:43:14 +0200

    createStationNameDictionary, save_tripData, Comments
    New method for generating a dictionary containing number and names of fixed stations. New method for saving the trips dataframe. Comments for understanding.

--- nextbike/io/input.py
+++ nextbike/io/input.py
@@ -3,7 +3,7 @@ import pandas as pd
 import os
 import pickle
 
-
+# load raw nextbike data as csv file from designated directory
 def read_file(path=os.path.join(get_data_path(), "input/inputData.csv")):
     try:
         df = pd.read_csv(path)
@@ -11,30 +11,60 @@ def read_file(path=os.path.join(get_data_path(), "input/inputData.csv")):
     except FileNotFoundError:
         print("Data file not found. Path was " + path)
 
+# preprocess the raw nextbike data with basic data cleaning techniques
 def preprocessData(df):
-    try:
-        df.dropna(inplace=True)
-        df.drop(labels='Unnamed: 0', axis=1, inplace=True)
-        df = df[(df['trip'] == 'start') |  (df['trip'] == 'end')]
-        df['p_number'] = df.p_number.astype('int64')
-    except:
-        print('Error in preprocessData')
+    # drop empty rows and unneccessary columns
+    df.dropna(inplace=True)
+    df.drop(columns=['Unnamed: 0', 'p_spot', 'p_place_type', 'p_uid', 'p_bikes', 'p_bike'], inplace=True)
+
+    # drop rows which are not a start or end of a trip
+    df = df[(df['trip'] == 'start') |  (df['trip'] == 'end')]
+
+    # convert column datatype
+    df['p_number'] = df.p_number.astype('int64')
+
     return df
 
-def createTrips(df):
-    # TODO automate saving? Or create extra funtion that saves the outcome.
+# create a dictionary which stores the names of the fixed stations for the p_number as key
+def createStationNameDictionary(df):
+    # generate array of unique place numbers
+    pNumbersUnique = df.p_number.unique()
 
+    # generate array of unique place names in the same order
+    pNamesUnique = []    
+    for i in pNumbersUnique:
+        pName = df[df['p_number'] == i].p_name.unique()[0]
+        pNamesUnique.append(pName)
+
+    # generate dictionary
+    pNameNumberDict = dict(zip(pNumbersUnique, pNamesUnique))
+
+    pNameNumberDict[0] = 'Kein offizieller Standort'
+
+    return pNameNumberDict
+
+# create dataframe which contains the nextbike data in trip format
+def createTrips(df):
+    # cast datatype to datetime
     df['datetime'] = pd.to_datetime(df['datetime'])
+    
     dfTest = df
     tripList = []
     savedRow = []
     errorlines = []
-    # Only creates a trip if a bike has a start and end point.
+    
+    # iterate over all rows of the dataframe
     for index, row in dfTest.iterrows():
+        
+        # check if trip is start ort end
         if row['trip'] == 'start':
             savedRow = row
         if row['trip'] == 'end':
+            
+            # check if trip start and end are in following rows have the same b_number
             if savedRow['b_number'] == row['b_number']:
+                
+                # create trip and append it
                 trip = {'bNumber': row['b_number'], 'sTime': savedRow['datetime'],
                         'eTime': row['datetime'], 'duration': (row['datetime'] - savedRow['datetime']),
                         'sLong': savedRow['p_lng'], 'sLat': savedRow['p_lat'],
@@ -42,15 +72,24 @@ def createTrips(df):
                         'weekend': (lambda x: True if x > 4 else False)(row['datetime'].dayofweek),
                         'bType': row['b_bike_type'], 'sPlaceNumber': savedRow['p_number'], 'ePlaceNumber': row['p_number']}
                 tripList.append(trip)
-                savedRow['b_number'] = -1
             else:
+                
+                # save rows with errors due to not fitting b_number
                 errorlines.append(index)
+            
+            # clean savedRow
+            savedRow['b_number'] = -1
+    
+    # print rows with errors
     if len(errorlines) > 0:
         print('Error at lines:')
         for line in errorlines:
             print(line)
+    
+    # create dataframe out of list
     dfTrip = pd.DataFrame(tripList, columns=['bNumber', 'sTime', 'eTime', 'duration', 'sLong', 'sLat', 'eLong', 'eLat',
                                              'weekend', 'bType', 'sPlaceNumber', 'ePlaceNumber'])
+    
     return dfTrip
 
 def read_model():
--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -2,6 +2,12 @@ from .utils import get_data_path
 import os
 import pickle
 
-
 def save_model(model):
     pickle.dump(model, open(os.path.join(get_data_path(), "output/model.pkl"), 'wb'))
+
+# save generated tripData as csv file to designated data directory
+def save_tripData(df, path=os.path.join(get_data_path(), "output/tripData.csv")):
+    try:
+        df.to_csv(path)
+    except:
+        print("Data file could not be saved. Path was " + path)
