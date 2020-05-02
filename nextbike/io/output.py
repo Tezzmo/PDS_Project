361c6628a29d62598d3711095c377a73273c603a
Tezzmo <lorenz.kriehn@gmail.com>
2020-05-02 11:46:17 +0200

    save output.py to merge

--- nextbike/io/output.py
+++ nextbike/io/output.py
@@ -1,7 +1,18 @@
 from .utils import get_data_path
 import os
 import pickle
+import pandas as pd
 
 
 def save_model(model):
     pickle.dump(model, open(os.path.join(get_data_path(), "output/model.pkl"), 'wb'))
+
+
+def saveTrips(df):
+    try:
+        df.to_csv(r'./data/output/tripData.csv')
+    except FileExistsError:
+        print('File already exists.')
+    except FileNotFoundError:
+        print('Can not find the file or folder.')
+
