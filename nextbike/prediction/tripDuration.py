9c1bb9a468616dea288166625feb97ad1df341c1
Tezzmo <lorenz.kriehn@gmail.com>
2020-06-10 13:13:29 +0200

    fix imports

--- nextbike/prediction/tripDuration.py
+++ nextbike/prediction/tripDuration.py
@@ -13,7 +13,7 @@ from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.externals.joblib import dump, load
+from joblib import dump, load
 from . import utils
 from .. import io
 import os
